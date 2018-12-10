6ec9f7c81bb39819818297fad3789b5e0c6ee288
GreatBahram <aghaee.bahram@gmail.com>
2018-12-10 17:15:31 +0330

    make everything ready for new release

--- CHANGES.md
+++ CHANGES.md
@@ -1,3 +1,10 @@
+## 0.2.0 (2018-12-10)
+
+- Write all shecan dns name servers into /etc/resolv.conf by default.
+- Raise notimplementationError for --permanent option.
+- Refactor and check all modules code styles.
+- Update `setup.py` file.
+
 ## 0.1.0 (2018-12-10)
 
 - Initial release.
--- src/shecan/__version__.py
+++ src/shecan/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'shecan'
 __description__ = 'The Shecan CLI is used to configure your DNS name server based on Shecan website from the command line.'
 __url__ = 'https://github.com/GreatBahram/shecan-cli'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'Bahram Aghaei'
 __author_email__ = 'aghaee.bahram@gmail.com'
 __license__ = 'Academic Free License, version 3'
