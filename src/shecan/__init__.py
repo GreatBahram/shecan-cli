3d28ae7bca89d10622cb865f7174d1d4d96877cd
GreatBahram <aghaee.bahram@gmail.com>
2019-11-18 15:11:03 +0330

    add config module inside __init__ file

--- src/shecan/__init__.py
+++ src/shecan/__init__.py
@@ -5,7 +5,16 @@ The Shecan CLI https://shecan.ir
 """
 
 from .api import current_dns, get, list_dns, start_dns_db, stop_dns_db, update
+from . import config
 
-__all__ = ["current_dns", "get", "list_dns", "start_dns_db", "stop_dns_db", "update"]
+__all__ = [
+    "current_dns",
+    "get",
+    "list_dns",
+    "start_dns_db",
+    "stop_dns_db",
+    "update",
+    "config",
+]
 
 version = "0.2.4"
