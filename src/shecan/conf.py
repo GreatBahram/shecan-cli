9f226b47b317e315f06e53f64bf58cf256d22a2d
Bahram Aghaei <aghaee.bahram@gmail.com>
2020-01-23 00:28:47 +0330

    Update confg file

--- src/shecan/conf.py
+++ src/shecan/conf.py
@@ -1,6 +1,7 @@
 """ Module containing classes to access the shecan configuration."""
 
 from configparser import ConfigParser
+from pathlib import Path
 
 
 class ShecanConfig:
@@ -8,30 +9,38 @@ class ShecanConfig:
 
     def __init__(self, config_file: str) -> None:
         self.config_file = config_file
+        self._parser = ConfigParser()
+        self._name = "shecan"
+        self._initialize()
 
-    def initialize(self):
+    def _initialize(self):
         """Initialize the shecan config file"""
-        pass
+        if "shecan" not in self._parser:
+            self._parser.add_section(self._name)
 
-    def read_config(self):
+    def _read_config(self):
         """ Read the configuration from a file."""
-        self.config = ConfigParser()
-        self.config.read(self.config_file)
+        self._parser.read(self.config_file)
 
-    def add(self, dns: dict):
+    def __enter__(self):
+        self._read_config()
+        return self
+
+    def __exit__(self, *exceptions):
+        with open(self.config_file, mode="wt") as fp:
+            self._parser.write(fp)
+
+    def update(self, ips: list):
         pass
 
     def list_dns(self):
-        """"""
-        pass
-
-    def update(self):
-        pass
+        """Return list of Shecan's dns servers."""
+        return [value for _, value in self._parser.items(self._name)]
 
     def delete(self,) -> None:
         """Remove all shecan's dns servers from configuratio."""
-        pass
+        self._parser.remove_section(self._name)
 
     def remove(self,) -> None:
         """Remove the configuration file."""
-        pass
+        Path(self.config_file).unlink()
