760331285b059542aea63bae363d79a31851f1be
Bahram Aghaei <aghaee.bahram@gmail.com>
2021-03-16 20:40:31 +0330

    use python builtin http client instead of requests library

--- src/shecan/utils.py
+++ src/shecan/utils.py
@@ -4,8 +4,7 @@ shecan.utils
 This module provides utility functions that are used within shecan.
 """
 from typing import Tuple
-
-import requests
+from urllib import request
 
 from bs4 import BeautifulSoup
 
@@ -15,8 +14,7 @@ CSS_SELECTOR = ".shecan-dns-ips"
 
 def get_shecan_ips(url: str = BASE_URL, selector: str = CSS_SELECTOR) -> Tuple[str]:
     """ Retrieve shecan DNS IP addresses."""
-    with requests.get(url) as req:
-        data = req.text
+    data = request.urlopen(BASE_URL).read().decode("utf-8")
     soup = BeautifulSoup(data, "lxml")
     ips = soup.select(selector)
     return tuple(item.string for item in ips)
